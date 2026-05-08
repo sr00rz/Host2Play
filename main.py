7f5dfc37112cf085ed170078b76a7b943aa00c95
strong76 <9119135@gmail.com>
2026-05-08 16:44:41 +0800

    Update main.py

--- main.py
+++ main.py
@@ -22,6 +22,7 @@ except ImportError:
 RENEW_URLS = [
     "https://host2play.gratis/server/renew?i=ce2880cc-2cb4-49f0-9101-abbc547e3560",
     "https://host2play.gratis/server/renew?i=8e54ba2e-acb1-4f45-9cb2-31bb28ab3b86",
+    "https://host2play.gratis/server/renew?i=13fa5c40-a2de-45e0-b794-54cb5b398e21",
     # 添加更多链接
 ]
 
